f66a0c3910a4cf75a25b6f2bd8ec633f68f413a5
Alexander Rødseth <rodseth@gmail.com>
2014-10-08 12:43:55 +0200

    Browserspeak was renamed

--- compileloop.sh
+++ compileloop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SOURCE=
-for f in *.go ../browserspeak/*.go ../genericsite/*.go ../instapage/*.go ../simpleredis/*.go ../siteengines/*.go; do
+for f in *.go ../onthefly/*.go ../genericsite/*.go ../instapage/*.go ../simpleredis/*.go ../siteengines/*.go; do
   SOURCE+="$f "
 done
 BIN=mosebarkweb
